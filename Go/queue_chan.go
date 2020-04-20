0098c7a4389bcc2d2f70fbaea99235f5d59f7d9b
Naveen Kaje <naveen.kaje@juul.com>
2020-04-20 15:25:47 -0500

    queue_chan: add file
    queue_chan uses a queue to enqueue items (can be a char, int...)

---
+++ Go/queue_chan.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+import "time"
+import "container/list"
+import "sync"
+
+type Txfn func() error
+
+func run_thread() error {
+
+    fmt.Println("processing done ")
+
+    return nil
+}
+
+func main() {
+    queue := list.New()
+    var mutex = &sync.Mutex{}
+    const sample ="abcdefghij"
+
+    var sem = make(chan int, 5)
+    fmt.Println("run main()")
+
+    for i:= 0; i<10; i++ {
+        fmt.Printf("queue %c\n", sample[i])
+        queue.PushBack(sample[i])
+        sem <- 1
+        go func(queue *list.List) {
+           mutex.Lock()
+           if (queue.Len() > 0) {
+                e := queue.Front()
+                queue.Remove(e)
+                fmt.Printf("dequeue %c\n", e.Value)
+           }
+           mutex.Unlock()
+           run_thread()
+            <-sem
+        } (queue)
+    }
+
+    fmt.Println("Waiting for 10 seconds")
+    time.Sleep(10 * time.Second)
+}
