cf3120ff5b3c02457c0f15d19b2c8e66d432a0b9
Naveen Kaje <naveen.kaje@juul.com>
2020-10-24 08:19:04 -0500

    Update/Format queue_chan.go

--- Go/queue_chan.go
+++ Go/queue_chan.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 import "time"
 import "container/list"
@@ -8,40 +9,40 @@ type Txfn func() error
 
 func run_thread() error {
 
-    fmt.Println("processing done ")
+	fmt.Println("processing done ")
 
-    return nil
+	return nil
 }
 
 func main() {
-    queue := list.New()
-    var mutex = &sync.Mutex{}
-    const sample = "abcdefghij"
+	queue := list.New()
+	var mutex = &sync.Mutex{}
+	const sample = "abcdefghij"
 
-    var sem = make(chan int, 5)
-    fmt.Println("run main()")
+	var sem = make(chan int, 5)
+	fmt.Println("run main()")
 
-    for i:= 0; i<10; i++ {
-        sem <- 1
+	for i := 0; i < 10; i++ {
+		sem <- 1
 
-        mutex.Lock()
-        fmt.Printf("queue %c, length of queue %d\n", sample[i], len(sem))
-        queue.PushBack(sample[i])
-        mutex.Unlock()
+		mutex.Lock()
+		fmt.Printf("queue %c, length of queue %d\n", sample[i], len(sem))
+		queue.PushBack(sample[i])
+		mutex.Unlock()
 
-        go func(queue *list.List) {
-           mutex.Lock()
-           if (queue.Len() > 0) {
-                e := queue.Front()
-                queue.Remove(e)
-                fmt.Printf("dequeue %c length of queue %d\n", e.Value, len(sem)-1)
-           }
-           mutex.Unlock()
-           run_thread()
-            <-sem
-        } (queue)
-    }
+		go func(queue *list.List) {
+			mutex.Lock()
+			if queue.Len() > 0 {
+				e := queue.Front()
+				queue.Remove(e)
+				fmt.Printf("dequeue %c length of queue %d\n", e.Value, len(sem)-1)
+			}
+			mutex.Unlock()
+			run_thread()
+			<-sem
+		}(queue)
+	}
 
-    fmt.Println("Waiting for 10 seconds")
-    time.Sleep(10 * time.Second)
+	fmt.Println("Waiting for 10 seconds")
+	time.Sleep(10 * time.Second)
 }
