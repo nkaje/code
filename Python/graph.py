707955499198ff470b6e3acca8cfb141ba7e9c85
Naveen Kaje <naveen.kaje@juul.com>
2020-11-09 16:04:54 -0600

    graph.py: Add distance methods

--- Python/graph.py
+++ Python/graph.py
@@ -22,12 +22,42 @@ def BFS(i):
     queue.insert(0, i)
     visited.append(i)
     while(len(queue) > 0):
-        n = queue.pop(0)
+        n = queue.pop(len(queue)-1)
         for j in range(0, N):
-            if (a[n][j] == 1) and visited.count(j) == 0:
+            if ( a[n][j] == 1) and visited.count(j) == 0:
                 queue.insert(0, j)
                 visited.append(j)
     print(visited)
+
+def BFS_path(s, e):
+    queue = []
+    dist = [-1 for i in range(N)]
+
+    queue.insert(0, s)
+    while(len(queue) > 0):
+        n = queue.pop(len(queue)-1)
+        for j in range(0, N):
+            if ( a[n][j] == 1):
+                if dist[j] == -1:
+                    queue.insert(0, j)
+                    dist[j] = dist[n]+1
+    print(dist)
+    return dist[e]
+
+def DFS_path(s, e):
+    stack = []
+    dist = [-1 for i in range(N)]
+
+    stack.insert(len(stack), s)
+    while(len(stack) > 0):
+        n = stack.pop(0)
+        for j in range(0, N):
+            if ( a[n][j] == 1):
+                if dist[j] == -1:
+                    stack.insert(len(stack), j)
+                    dist[j] = dist[n]+1
+    print(dist)
+    return dist[e]
 
 def DFS_I(i):
     stack = []
@@ -60,10 +90,10 @@ def DFS(i):
 if __name__ == "__main__":
     add_edge(0, 1)
     add_edge(0, 2)
-    add_edge(2, 6)
     add_edge(0, 3)
-    add_edge(1, 4)
+    add_edge(0, 4)
     add_edge(4, 5)
+
     print_matrix()
     print("BFS")
     BFS(0)
@@ -73,3 +103,10 @@ if __name__ == "__main__":
 
     print("DFS")
     DFS(0)
+
+    print("BFS path 0 -> 4")
+    ret = BFS_path(0, 5)
+    print(ret + 1)
+    print("DFS path 0 -> 4")
+    ret = DFS_path(0, 5)
+    print(ret + 1)
